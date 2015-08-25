ae1243201225ff2b8f4128ec3a0554f08e1f4534
xzjs <2310976219@qq.com>
2015-08-25 13:57:35 +0800

    设置github weebhooks

--- hook.php
+++ hook.php
@@ -5,10 +5,17 @@
  * Date: 15/8/25
  * Time: 上午11:18
  */
-error_reporting ( E_ALL );
-$dir = 'D:\\xampp\\htdocs\wifi\\';//该目录为git检出目录
-$handle = popen('cd '.$dir.' && git pull 2>&1','r');
-$read = stream_get_contents($handle);
-printf($read);
-pclose($handle);
-echo 'hello';
+header("Content-type: text/html; charset=utf-8");
+$js=json_decode($_REQUEST["hook"]);//json转换
+if($js->password!="123")die("ERROR!");//判断密码
+$fp=fopen("D:/log.txt",'a');
+$lastcommit=$js->push_data->commits[count($js->push_data->commits)-1];//获取最后的commit
+if(strstr($lastcommit->message,"release"))//这里意为：如果最后的commit包含"release"则进行自动发布。
+{
+    exec('cd D:/xampp/htdocs/wifi git pull origin master');//进入目录
+    fwrite($fp,"!!!".date('Y-m-d H:i:s')."\t".$lastcommit->message."\t".$lastcommit->author->name."\t"."Y"."\n");//进行记录
+}
+else
+{
+    fwrite($fp,date('Y-m-d H:i:s')."\t".$lastcommit->message."\t".$lastcommit->author->name."\t"."N"."\n");
+}
