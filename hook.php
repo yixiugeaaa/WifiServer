f62cf3caea147239033aa87d76166fd5e5d1078e
xzjs <2310976219@qq.com>
2015-08-25 14:59:39 +0800

    设置github weebhooks

--- hook.php
+++ hook.php
@@ -5,7 +5,10 @@
  * Date: 15/8/25
  * Time: 上午11:18
  */
-exec("cd d:/xampp/htdocs/wifi/ git pull ",$output);
+//exec("cd d:/xampp/htdocs/wifi/ git pull ",$output);
+exec("D:");
+exec("cd xampp/htdocs/wifi");
+exec("git pull",$output);
 $myfile=fopen("log.txt","w");
 foreach ($output as $out) {
     fwrite($myfile,$out."\n");
