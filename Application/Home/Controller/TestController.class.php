5dd810bcbaf9cc05a940b3f299ce4bbf7d3b6a05
xzjs <2310976219@qq.com>
2015-09-14 14:40:24 +0800

    增加获取ip测试

---
+++ Application/Home/Controller/TestController.class.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xzjs
+ * Date: 15/8/18
+ * Time: 下午9:52
+ */
+namespace Home\Controller;
+
+use Think\Controller;
+
+class TestController extends Controller
+{
+    public function index(){
+        $ip = $_SERVER["REMOTE_ADDR"];
+        echo $ip;
+    }
+}
