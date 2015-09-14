fe15343d4a0921c437cff37a04912e1b735285df
xzjs <2310976219@qq.com>
2015-09-14 15:08:55 +0800

    增加获取ip测试

--- Application/Home/Controller/TestController.class.php
+++ Application/Home/Controller/TestController.class.php
@@ -14,7 +14,10 @@ class TestController extends Controller
     public function index(){
         $ip = $_SERVER["REMOTE_ADDR"];
         //echo $ip;
-        $data = @file_get_contents("http://ip.taobao.com/service/getIpInfo.php?ip=".$ip);
-        echo json_decode($data,true);
+        $data = @file_get_contents("http://int.dpool.sina.com.cn/iplookup/iplookup.php?format=json&ip=".$ip);
+        //var_dump($data);
+        var_dump($data);
+        echo $data['lo'].' '.$data['lc'];
+        //echo json_encode($data);
     }
 }
